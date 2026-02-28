7d537581d5b903a6689520504d255dd7016c72a0
maryamattique034-spec <maryamattique034@gmail.com>
2026-02-28 14:17:37 +0500

    modified version

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&lac88l394+v3+b9rq9ei!te+_6o(m1h(#on4k2b2+6l4wm27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
